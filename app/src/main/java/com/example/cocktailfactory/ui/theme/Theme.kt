8c749797184aa8c71fb50bfc1909c22e810810db
Szabo Liza <liza.szabo0715@yahoo.ro>
2023-04-12 10:14:42 +0200

    Add hilt, room and retrofit2 dependencies

--- app/src/main/java/com/example/cocktailfactory/ui/theme/Theme.kt
+++ app/src/main/java/com/example/cocktailfactory/ui/theme/Theme.kt
@@ -28,10 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun CocktailFactoryTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit
-) {
+fun CocktailFactoryTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
