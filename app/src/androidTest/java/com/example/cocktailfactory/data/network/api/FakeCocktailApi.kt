a7cef975eb956dc640d75854fc80db5204a24cdd
Szabo Liza <liza.szabo0715@yahoo.ro>
2023-04-21 23:37:45 +0200

    Add methods to FakeCocktailApi.kt

--- app/src/androidTest/java/com/example/cocktailfactory/data/network/api/FakeCocktailApi.kt
+++ app/src/androidTest/java/com/example/cocktailfactory/data/network/api/FakeCocktailApi.kt
@@ -1,5 +1,6 @@
 package com.example.cocktailfactory.data.network.api
 
+import com.example.cocktailfactory.data.network.model.CocktailRequest
 import com.example.cocktailfactory.data.network.model.GetCocktailsResponse
 import com.google.gson.Gson
 import javax.inject.Inject
@@ -8,6 +9,14 @@ import javax.inject.Singleton
 @Singleton
 class FakeCocktailApi @Inject constructor(private val gson: Gson) : CocktailManagerApi {
     override suspend fun getCocktails(): GetCocktailsResponse {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun saveCocktail(cocktailRequest: CocktailRequest) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteCocktail(id: String) {
         TODO("Not yet implemented")
     }
 }
--- app/src/main/java/com/example/cocktailfactory/data/network/api/CocktailManagerApi.kt
+++ app/src/main/java/com/example/cocktailfactory/data/network/api/CocktailManagerApi.kt
@@ -1,9 +1,18 @@
 package com.example.cocktailfactory.data.network.api
 
+import androidx.room.Delete
+import com.example.cocktailfactory.data.network.model.CocktailRequest
 import com.example.cocktailfactory.data.network.model.GetCocktailsResponse
 import retrofit2.http.GET
+import retrofit2.http.PUT
 
 interface CocktailManagerApi {
     @GET("random.php")
     suspend fun getCocktails(): GetCocktailsResponse
+
+    @PUT
+    suspend fun saveCocktail(cocktailRequest: CocktailRequest)
+
+    @Delete
+    suspend fun deleteCocktail(id: String)
 }
