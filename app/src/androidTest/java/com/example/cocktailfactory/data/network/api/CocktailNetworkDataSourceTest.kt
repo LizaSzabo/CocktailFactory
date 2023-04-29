cef1f981594a5a314bdfc12a195978858dbeb7c8
Szabo Liza <liza.szabo0715@yahoo.ro>
2023-04-29 12:06:48 +0200

    Add network dataset sample tests

--- app/src/androidTest/java/com/example/cocktailfactory/data/network/api/CocktailNetworkDataSourceTest.kt
+++ app/src/androidTest/java/com/example/cocktailfactory/data/network/api/CocktailNetworkDataSourceTest.kt
@@ -3,37 +3,42 @@ package com.example.cocktailfactory.data.network.api
 import com.example.cocktailfactory.data.network.api.mock.FakeCocktailApi
 import com.example.cocktailfactory.data.network.source.CocktailNetworkDataSource
 import com.example.cocktailfactory.data.network.util.NetworkResult
-import dagger.hilt.android.testing.HiltAndroidRule
-import dagger.hilt.android.testing.HiltAndroidTest
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertTrue
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
-import javax.inject.Inject
+import java.net.SocketTimeoutException
+import kotlin.test.assertFailsWith
 
-@HiltAndroidTest
 class CocktailNetworkDataSourceTest {
 
-    @get:Rule
-    val hiltRule = HiltAndroidRule(this)
+    private val gson: Gson = GsonBuilder().create()
 
-    @Inject
-    lateinit var fakeCocktailApi: FakeCocktailApi
+    private var fakeCocktailApi = FakeCocktailApi(gson)
 
     private val cocktailNetworkDataSource = CocktailNetworkDataSource(fakeCocktailApi)
 
     @Before
     fun setUp() {
-        hiltRule.inject()
         fakeCocktailApi.isNetworkAvailable = true
         fakeCocktailApi.isSuccess = true
     }
 
     @Test
     fun getCocktailQueryWorks() = runBlocking {
-        val actualResponse = cocktailNetworkDataSource.getCocktails()
+        val response = cocktailNetworkDataSource.getCocktails()
+        assertTrue(response is NetworkResult)
+    }
 
-        assertTrue(actualResponse is NetworkResult)
+    @Test
+    fun getCocktailQueryNoInternetError() = runBlocking {
+        fakeCocktailApi.isNetworkAvailable = false
+
+        val exception = assertFailsWith(
+            exceptionClass = SocketTimeoutException::class,
+            block = { cocktailNetworkDataSource.getCocktails() }
+        )
     }
 }
